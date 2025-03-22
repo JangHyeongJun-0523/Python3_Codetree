c25f9ebf83b44e9cfc05e9202008683650257b61
Jang Hyung Jun <70615068+JangHyeongJun-0523@users.noreply.github.com>
2025-03-22 21:22:17 +0900

    Title: 비교 연산, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250322/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+A, B=input().split()
+A=int(A)
+B=int(B)
+
+if A>=B:
+    print(1)
+else:
+    print(0)
+
+if A>B:
+    print(1)
+else:
+    print(0)
+
+if A<=B:
+    print(1)
+else:
+    print(0)
+
+if A<B:
+    print(1)
+else:
+    print(0)
+
+if A==B:
+    print(1)
+else:
+    print(0)
+
+if A!=B:
+    print(1)
+else:
+    print(0)
