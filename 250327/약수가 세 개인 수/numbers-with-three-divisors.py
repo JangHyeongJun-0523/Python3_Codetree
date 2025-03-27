a319307ab06b15210c7bebe457a4b90f9db82099
Jang Hyung Jun <70615068+JangHyeongJun-0523@users.noreply.github.com>
2025-03-27 15:33:34 +0900

    Title: 약수가 세 개인 수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250327/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250327/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,8 +2,9 @@ start, end = map(int, input().split())
 cnt = int(0)
 
 for i in range(start, end+1):
-    if (i ** (1/2)) % 1 == 0:
-        if (i ** (1/2)) % 2 == 0 and i ** (1/2) != 2:
-            continue
-        cnt += 1
+    if i != 1:
+        if (i ** (1/2)) % 1 == 0:
+            if (i ** (1/2)) % 2 == 0 and i ** (1/2) != 2:
+                continue          
+            cnt += 1
 print(cnt)
